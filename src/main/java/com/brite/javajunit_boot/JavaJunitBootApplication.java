806b81eb40adebd3ba34eae2cc7b0bc96f3db6ba
mveluru <mveluru@gmail.com>
2024-05-12 20:12:17 -0500

    purpose - how to create test cases

---
+++ src/main/java/com/brite/javajunit_boot/JavaJunitBootApplication.java
@@ -0,0 +1,13 @@
+package com.brite.javajunit_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJunitBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaJunitBootApplication.class, args);
+    }
+
+}
